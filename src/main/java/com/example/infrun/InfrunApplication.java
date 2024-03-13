0dc56b88fa437320000928975a1987ffd75d1348
지현진 <nhjin1106@gmail.com>
2024-03-13 20:09:50 +0900

    커밋테스트

---
+++ src/main/java/com/example/infrun/InfrunApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrunApplication.class, args);
+    }
+
+}
